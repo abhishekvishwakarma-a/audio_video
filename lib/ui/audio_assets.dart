e566820451b5e7540380b469c5eba90500fd0eaf
abhishekvishwakarma-a <abhishek0542.v@gmail.com>
2021-07-17 21:22:00 +0530

    first commit

--- lib/ui/audio_assets.dart
+++ lib/ui/audio_assets.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:audioplayers/audioplayers.dart';
-import 'package:audioplayers/audio_cache.dart';
+//import 'package:audioplayers/audio_cache.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 AudioPlayer newPlayer = new AudioPlayer();
